5aff2ba400b1c083581ce7404f3ddd15e7525d8c
Данијел Митровић <rodjenihm@outlook.com>
2020-02-15 21:13:39 +0100

    Add Roles table to database

---
+++ ZrakStore.Database/dbo/Tables/Roles.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Roles] (
+    [Id]   NVARCHAR (450) NOT NULL,
+    [CreatedAt]    DATETIME2 (7)  NOT NULL,
+    [Name] NVARCHAR (256) NOT NULL,
+    CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Roles_Name]
+    ON [dbo].[Roles]([Name] ASC);
