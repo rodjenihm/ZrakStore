b44196b635c56f8db253a03fe48cee0e0c15a5e0
Данијел Митровић <rodjenihm@outlook.com>
2020-02-15 21:12:23 +0100

    Add Users table to database

---
+++ ZrakStore.Database/dbo/Users.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           NVARCHAR (450) NOT NULL,
+    [CreatedAt]    DATETIME2 (7)  NOT NULL,
+    [Username]     NVARCHAR (450) NOT NULL,
+    [PasswordHash] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Users_Username]
+    ON [dbo].[Users]([Username] ASC);
