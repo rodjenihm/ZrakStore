e16ba232ef86ba1bd37838d0b7e92731902890fd
Данијел Митровић <rodjenihm@outlook.com>
2020-02-16 00:12:46 +0100

    Add services to ZrakStore.Services project

---
+++ ZrakStore.Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZrakStore.Data.Entities;
+
+namespace ZrakStore.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByUsernameAsync(string username);
+        Task<User> GetUserByIdAsync(string id);
+        Task AddUserAsync(User user);
+    }
+}
---
+++ ZrakStore.Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZrakStore.Data.Entities;
+using ZrakStore.Data.Repositories;
+
+namespace ZrakStore.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IAsyncUserRepository userRepository;
+
+        public UserService(IAsyncUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await userRepository.AddAsync(user);
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await userRepository.GetAllAsync();
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+            return await userRepository.GetByUsernameAsync(username);
+        }
+
+        public async Task<User> GetUserByIdAsync(string id)
+        {
+            return await userRepository.GetByIdAsync(id);
+        }
+    }
+}
