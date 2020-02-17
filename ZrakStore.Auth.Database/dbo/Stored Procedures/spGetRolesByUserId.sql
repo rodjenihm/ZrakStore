9e90aea820f9120e2259f41f9075c67ec7b4eaae
Данијел Митровић <rodjenihm@outlook.com>
2020-02-17 17:52:44 +0100

    Add Stored Procedures to find user roles by username and id

---
+++ ZrakStore.Auth.Database/dbo/Stored Procedures/spGetRolesByUserId.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spGetRolesByUserId]
+	@UserId NVARCHAR (450)
+AS
+BEGIN
+	SELECT RoleId
+	FROM dbo.Users
+	JOIN dbo.UserRoles
+	ON (dbo.Users.Id = dbo.UserRoles.UserId)
+	WHERE dbo.Users.Id = @UserId;
+END
---
+++ ZrakStore.Auth.Database/dbo/Stored Procedures/spGetRolesByUsername.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spGetRolesByUsername]
+	@Username NVARCHAR (450)
+AS
+BEGIN
+	SELECT RoleId
+	FROM dbo.Users
+	JOIN dbo.UserRoles
+	ON (dbo.Users.Id = dbo.UserRoles.UserId)
+	WHERE dbo.Users.UserName = @Username;
+END
