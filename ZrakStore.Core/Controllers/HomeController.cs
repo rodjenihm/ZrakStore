c75ffa8c25a0f79383dfc33989e5c1fd8be5239c
Данијел Митровић <rodjenihm@outlook.com>
2020-02-21 06:03:43 +0100

    Code Cleanup

--- ZrakStore.Core/Controllers/HomeController.cs
+++ ZrakStore.Core/Controllers/HomeController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace ZrakStore.Core.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public IActionResult Index()
         {
+            ViewData["TokenEndpoint"] = configuration["TokenEndpoint"];
             return View();
         }
 
